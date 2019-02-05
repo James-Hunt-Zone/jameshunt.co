7e748959d284940ce9ad8b62d7fce4e4e2475fc1
James Hunt <jameshunt.co@gmail.com>
2019-02-05 22:51:39 +0000

    Adding section body style and storybook module.

--- src/components/sectionBody/index.js
+++ src/components/sectionBody/index.js
@@ -2,18 +2,18 @@ import React from "react"
 import styles from './styles.module.scss';
 
 class SectionBody extends React.Component {
-    render() {
-      return (
-        <section>      
-          <div class="grid-item grid-item--narrow">
-            <h3>Info</h3>
-          </div>
-          <div class="grid-item grid-item--wide">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ornare, urna sit amet varius consectetur, arcu mauris ultricies mi, sed lacinia odio est in tortor. Quisque vel turpis posuere, consequat lacus ut, maximus massa. Suspendisse ac libero ac est aliquam pellentesque id nec lectus. Suspendisse egestas tristique faucibus. Ut nunc ipsum, pretium et turpis at, mollis sagittis lacus. Pellentesque porta congue erat eget imperdiet. Sed id eros ac metus lobortis finibus eu vitae ligula. Vestibulum vulputate justo sit amet nunc lobortis, ut ultrices magna luctus. Vivamus porta erat sed iaculis sollicitudin. Nunc congue lorem vitae ex volutpat, vel porttitor nunc rutrum.</p>
-          </div>
-        </section>
-      );
-    }
+  render() {
+    return (
+      <section class="border-top border-bottom">      
+        <div class="grid-item grid-item--narrow">
+          <h3 class="section-title">{this.props.title}</h3>
+        </div>
+        <div class="grid-item grid-item--wide">
+          <p class="section-body">{this.props.body}</p>
+        </div>
+      </section>
+    );
   }
+}
 
-  export default SectionBody;
+export default SectionBody;
--- src/components/sectionBody/story.js
+++ src/components/sectionBody/story.js
@@ -3,9 +3,15 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 // import { action } from '@storybook/addon-actions';
 // import { linkTo } from '@storybook/addon-links';
-import { withKnobs } from '@storybook/addon-knobs/html';
+import { withKnobs, text } from '@storybook/addon-knobs/html';
 import SectionBody from './index';
 
 storiesOf('Section Body', module)
   .addDecorator(withKnobs)
-  .add('default', () => <SectionBody />);
+  .add('default', () => <SectionBody />)
+  .add('default', () => {
+    const title = text('Title', 'Tech used');
+    const body = text('Body', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ornare, urna sit amet varius consectetur, arcu mauris ultricies mi, sed lacinia odio est in tortor. Quisque vel turpis posuere, consequat lacus ut, maximus massa. Suspendisse ac libero ac est aliquam pellentesque id nec lectus. Suspendisse egestas tristique faucibus. Ut nunc ipsum, pretium et turpis at, mollis sagittis lacus. Pellentesque porta congue erat eget imperdiet. Sed id eros ac metus lobortis finibus eu vitae ligula. Vestibulum vulputate justo sit amet nunc lobortis, ut ultrices magna luctus. Vivamus porta erat sed iaculis sollicitudin. Nunc congue lorem vitae ex volutpat, vel porttitor nunc rutrum.');
+
+    return <SectionBody title={title} body={body} />;
+  });

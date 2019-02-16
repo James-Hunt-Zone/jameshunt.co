bd7649a1013d54ccc8ff6aca1ee31a51d4af6e5c
James Hunt <jameshunt.co@gmail.com>
2019-02-16 08:43:21 +0000

    Added themes to the ribbon

--- src/components/projectImage/index.js
+++ src/components/projectImage/index.js
@@ -3,9 +3,11 @@ import Img from "gatsby-image"
 
 class ProjectImage extends React.Component {
   render() {
+    let className = 'project-ribbon project-ribbon--' + this.props.theme;
+
     return (
       <section className="section section--top">
-        <div className="project-ribbon"></div>
+        <div className={className}></div>
         <div className="col col--span2 col--offset1">
           <Img fluid={this.props.img} />
         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default ({data}) => {
       <main className="projects">
         {data.allProjectsJson.edges.map((item, index) => 
           <article className="project" key={index}>
-            <ProjectImage img={item.node.img.childImageSharp.fluid}></ProjectImage>
+            <ProjectImage img={item.node.img.childImageSharp.fluid} theme={item.node.theme}></ProjectImage>
             <ProjectHeader title={item.node.title}></ProjectHeader>
             <ProjectBody type="body" title="About" body={item.node.bio}></ProjectBody>
             <ProjectBody type="tech" title="Tech" tech={item.node.tech}></ProjectBody>
@@ -34,6 +34,7 @@ export const query = graphql`
       edges {
         node {
           title
+          theme
           bio
           tech
           img {
